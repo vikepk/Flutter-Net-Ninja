b846597eca3d49fc550ef762a3563c317cd05266
vikepk <prempk0516@gmail.com>
2023-03-07 15:26:01 +0530

    Stateless

--- lib/add.dart
+++ lib/add.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/src/widgets/framework.dart';
+
 import 'main.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
